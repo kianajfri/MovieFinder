d962aa371df7c2ae0a16ddba890a7a248d193445
Kiana <Kianajafari100@gmail.com>
2025-12-04 22:05:32 +0330

    Render movies in home page

--- js/app.js
+++ js/app.js
@@ -1,15 +1,30 @@
-import { searchShows, showSingleSearch, showsPage } from "./api.js";
+import {showsPage } from "./api.js";
 
-async function testAPI() {
-  const search = await searchShows("friends");
-  console.log("Search:", search);
+async function loadHomeMovies() {
+  const movieGrid = document.getElementById("movieGrid");
 
-  const singleSearch = await showSingleSearch("girls");
-  console.log("singleSearch:", search);
+  const data = await showsPage(0);
 
+  if (!data) {
+    movieGrid.innerHTML = "<p>Error loading movies</p>";
+    return;
+  }
 
-  const page = await showsPage(3);
-  console.log("Shows page:", page);
+  data.forEach(show => {
+    const card = document.createElement("div");
+    card.className = "movie-card";
+    card.innerHTML = `
+      <img src="${show.image?.medium || './assets/no-image.png'}" alt="${show.name}" />
+      <div class="movie-card-info">
+        <h4>${show.name}</h4>
+        <span>${show.premiered || "Unknown"}</span>
+      </div>
+    `;
+      card.addEventListener("click", () => {
+      window.location.href = `details.html?id=${show.id}`;
+    });
+
+    movieGrid.appendChild(card);
+  });
 }
-
-testAPI();
+loadHomeMovies();
